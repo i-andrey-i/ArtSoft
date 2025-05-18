ea1f738964185d7bf44d1d9d8182064dc990b38b
tarkue <mrfrostlikchannel@gmail.com>
2025-05-19 02:52:09 +0500

    Rename MessageService to FileService for clarity and consistency in service naming

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs'
 
 
 @Injectable({ providedIn: 'root' })
-export class MessageService extends BaseHttpService {
+export class FileService extends BaseHttpService {
     constructor() {
         super()
     }
